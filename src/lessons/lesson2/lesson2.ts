214a89b0e1a138975526f7e1c9bd1ce088c2e7bb
Fristail27 <fristail27@gmail.com>
2021-01-18 18:49:29 +0300

    2-les

--- src/lessons/lesson2/lesson2.ts
+++ src/lessons/lesson2/lesson2.ts
@@ -24,6 +24,14 @@ console.log('lesson 2');
 // Task 01
 // Реализовать функцию sum которая суммирует 2 числа следующим образом sum(3)(6) === 9
 
+// function sum(n:number) {
+//     return function (n2:number) {
+//         return n+n2
+//     }
+// }
+//
+// console.log(sum(2)(3))
+
 // Task 02
 // Реализовать функцию makeCounter которая работает следующим образом:
 // const counter = makeCounter();
@@ -33,13 +41,59 @@ console.log('lesson 2');
 // counter2(); // 1
 // counter(); // 3
 
+// function makeCounter() {
+//     let count = 0
+//     return function () {
+//         return ++count
+//     }
+// }
+//
+// const counter = makeCounter()
+// console.log(counter())
+// console.log(counter())
+// console.log(counter())
+// const counter2 = makeCounter()
+// console.log(counter2())
+// console.log(counter())
+
 // Task 03
 // Переписать функцию из Task 02 так, что бы она принимала число в качестве аргумента и это число было стартовым значением счетчика
 // и возвращала следующий объект методов:
 // increase: +1
 // decrease: -1
-// reset: установить счетчик в 0;
+// rest: установить счетчик в 0;
 // set: установить счетчик в заданное значение;
+
+// function makeCounter(n:number) {
+//     let count = 0
+//     return {
+//         increase: ()=> ++count,
+//         decrease: ()=> --count,
+//         rest () {
+//             count = 0
+//             return count
+//         },
+//         set: (num:number) => {
+//             count = num
+//             return num
+//         }
+//     }
+// }
+//
+// let counter = makeCounter(10)
+// console.log(counter.increase())
+// console.log(counter.rest())
+
+
+// function superSum(n:number):number {
+//     if(n===1) {
+//         return n
+//     }
+//     return n + superSum(n-1)
+//
+// }
+//
+// console.log(superSum(100))
 
 // Task 04*
 // Реализовать функцию superSum которая принимает число в качестве аргумента, которое указывает на количество слагаемых
@@ -51,7 +105,27 @@ console.log('lesson 2');
 // 5) superSum(3)(2,5)(3) //10
 // 6) superSum(3)(2,5)(3,9) //10
 
+
 // P.S. типизируйте только аргументы, а при вызове функции используйте @ts-ignore
+
+
+function superSum(n:number) {
+    if(n===0) return 0
+    if(n===1) return (num:number) => num
+    let _arguments: number[] = []
+
+    function helper(...args:number[]) {
+        _arguments.length = n
+        _arguments= [..._arguments, ...args]
+        if(_arguments.length >=n) {
+            return _arguments.reduce((acc, numb)=>acc + numb)
+        } else {
+            return  helper
+        }
+
+    }
+    return helper
+}
 
 // Task 05
 // решить все задачи по рекурсии которые даны в конце статьи https://learn.javascript.ru/recursion
